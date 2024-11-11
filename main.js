0a18e750c7a1529e7fc55aaebca34f95bf081d1b
Nino <ninoxin2003@mail.ru>
2024-11-11 21:06:19 +0300

    link script

---
+++ main.js
@@ -0,0 +1,20 @@
+let a = "10";
+const b = 15;
+
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(a + b)
+console.log(b - a)
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+
+alert('привет!')
+confirm('Ты точно хочешь выйти?')
+let result = prompt('Сколько тебе лет?')
+
+console.log(result);
+
+
+
